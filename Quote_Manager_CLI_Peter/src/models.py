dc45a00e0523f1ed7850d235f1c2185416cf087b
unknown <peteradekolu@gmail.com>
2024-09-09 15:39:37 +0100

    adding src\models.py

--- Quote_Manager_CLI_Peter/src/models.py
+++ Quote_Manager_CLI_Peter/src/models.py
@@ -1,12 +1,26 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from typing import Optional
 
-Base = declarative_base()   
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
 
 class Quote(Base):
+
     __tablename__ = "quotes"
-    
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    category = Column(String)
-    author = Column(String)
-    quote = Column(String)
+
+    id: int = Column(Integer, primary_key=True, index=True)
+    category: Optional[str] = Column(String(50), nullable=False)
+    author: str = Column(String(100), nullable=False)
+    quote: str = Column(String(500), nullable=False)
+    created_at: DateTime = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at: DateTime = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    def __repr__(self) -> str:
+        return (
+            f"<Quote(id={self.id}, author='{self.author}', category='{self.category}')>"
+        )
