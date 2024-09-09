0fe42d981d212376ccf370003e9ac8d4c8276583
unknown <peteradekolu@gmail.com>
2024-09-09 15:54:31 +0100

    adding tests\test_database.py

--- Quote_Manager_CLI_Peter/tests/test_database.py
+++ Quote_Manager_CLI_Peter/tests/test_database.py
@@ -1,61 +1,125 @@
+from typing import Generator
+
 import pytest
-from src.database import get_db, db_path
-from src.models import Quote,Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from src.database import db_path, get_db
+from src.models import Base, Quote
 
-TEST_DB_PATH = 'test_quotes.db'
+TEST_DB_PATH = "test_quotes.db"
+
 
 @pytest.fixture(scope="module")
 def test_db():
-    """This function tests the database engine"""
-    test_engine  = create_engine(f'sqlite:///{TEST_DB_PATH}')
+
+    """
+    This function tests the database engine
+
+    Yields:
+    engine: A sqlalchemy engine object
+
+    """
+    test_engine = create_engine(f"sqlite:///{TEST_DB_PATH}")
     Base.metadata.create_all(test_engine)
 
     yield test_engine
 
     Base.metadata.drop_all(test_engine)
 
+
 @pytest.fixture(scope="function")
-def db_session(test_db):
-    """This function provides a session for the test database"""
+def db_session(test_db: create_engine) -> Generator[Session, None, None]:
+
+    """
+    This function provides a session for the test database
+
+    Args:
+    test_db: A sqlalchemy engine object
+
+    Yields:
+    session: A sqlalchemy session object
+
+    """
     with get_db() as session:
         yield session
 
 
-def test_database_connection(db_session):
-    """Test the database connection"""
+def test_database_connection(db_session) -> None:
+
+    """
+    Test the database connection
+
+    Returns: None
+
+    """
     assert isinstance(db_session, Session)
 
-def test_add_quote(db_session):
-    """Test adding a quote to the database"""
-    new_quote = Quote(quote="This is a test quote", author="Test Author", category="Test Category")
+
+def test_add_quote(db_session) -> None:
+
+    """
+    Test adding a quote to the database
+
+    Returns: None
+
+    """
+    new_quote = Quote(
+        quote="This is a test quote", author="Test Author", category="Test Category"
+    )
     db_session.add(new_quote)
     db_session.commit()
-    
-    retrieved_quote = db_session.query(Quote).filter(Quote.author == "Test Author").first()
+
+    retrieved_quote = (
+        db_session.query(Quote).filter(Quote.author == "Test Author").first()
+    )
     assert retrieved_quote is not None
     assert retrieved_quote.quote == "This is a test quote"
     assert retrieved_quote.category == "Test Category"
 
-def test_retrieve_quote(db_session):
-    """Test retrieving a quote from the database"""
-    new_quote = Quote(quote="This is a retrieved quote", author="Retrieved Peter", category="Retrieved Category")
+
+def test_retrieve_quote(db_session) -> None:
+
+    """
+    Test retrieving a quote from the database
+
+    Returns: None
+
+    """
+    new_quote = Quote(
+        quote="This is a retrieved quote",
+        author="Retrieved Peter",
+        category="Retrieved Category",
+    )
     db_session.add(new_quote)
     db_session.commit()
 
-    retrieved_quote = db_session.query(Quote).filter(Quote.author == "Retrieved Peter").first()
+    retrieved_quote = (
+        db_session.query(Quote).filter(Quote.author == "Retrieved Peter").first()
+    )
     assert retrieved_quote is not None
     assert retrieved_quote.quote == "This is a retrieved quote"
     assert retrieved_quote.category == "Retrieved Category"
 
-def test_update_quote(db_session):
-    """Test updating a quote in the database"""
-    new_quote = Quote(quote="This is an updated quote", author="Updated Peter", category="Updated Category")
+
+def test_update_quote(db_session) -> None:
+
+    """
+    Test updating a quote in the database
+
+    Returns: None
+
+    """
+    new_quote = Quote(
+        quote="This is an updated quote",
+        author="Updated Peter",
+        category="Updated Category",
+    )
     db_session.add(new_quote)
     db_session.commit()
 
-    retrieved_quote = db_session.query(Quote).filter(Quote.author == "Updated Peter").first()
+    retrieved_quote = (
+        db_session.query(Quote).filter(Quote.author == "Updated Peter").first()
+    )
     assert retrieved_quote is not None
     assert retrieved_quote.quote == "This is an updated quote"
     assert retrieved_quote.category == "Updated Category"
@@ -63,18 +127,33 @@ def test_update_quote(db_session):
     retrieved_quote.quote = "This is the latest updated quote"
     db_session.commit()
 
-    updated_quote = db_session.query(Quote).filter(Quote.author == "Updated Peter").first()
+    updated_quote = (
+        db_session.query(Quote).filter(Quote.author == "Updated Peter").first()
+    )
     assert updated_quote is not None
     assert updated_quote.quote == "This is the latest updated quote"
     assert updated_quote.category == "Updated Category"
 
-def test_delete_quote(db_session):
-    """Test deleting a quote from the database"""
-    new_quote = Quote(quote="This is a deleted quote", author="Deleted Peter", category="Deleted Category")
+
+def test_delete_quote(db_session) -> None:
+
+    """
+    Test deleting a quote from the database
+
+    Returns: None
+
+    """
+    new_quote = Quote(
+        quote="This is a deleted quote",
+        author="Deleted Peter",
+        category="Deleted Category",
+    )
     db_session.add(new_quote)
     db_session.commit()
 
-    retrieved_quote = db_session.query(Quote).filter(Quote.author == "Deleted Peter").first()
+    retrieved_quote = (
+        db_session.query(Quote).filter(Quote.author == "Deleted Peter").first()
+    )
     assert retrieved_quote is not None
     assert retrieved_quote.quote == "This is a deleted quote"
     assert retrieved_quote.category == "Deleted Category"
@@ -82,47 +161,85 @@ def test_delete_quote(db_session):
     db_session.delete(retrieved_quote)
     db_session.commit()
 
-    deleted_quote = db_session.query(Quote).filter(Quote.author == "Deleted Peter").first()
+    deleted_quote = (
+        db_session.query(Quote).filter(Quote.author == "Deleted Peter").first()
+    )
     assert deleted_quote is None
 
-def test_db_context_manager():
-    """Test the database context manager (get_db)"""
+
+def test_db_context_manager() -> None:
+
+    """
+    Test the database context manager (get_db)
+
+    Returns: None
+
+    """
     with get_db() as db:
         assert isinstance(db, Session)
         assert db.bind.url.database == db_path
 
 
-def test_db_context_manager_error():
+def test_db_context_manager_error() -> None:
+
+    """
+    Test the database context manager (get_db) with an error
+
+    Returns: None
+
+    """
     with pytest.raises(Exception):
         with get_db() as db:
             assert isinstance(db, Session)
             assert db.bind.url.database == db_path
             raise Exception("An error occurred")
-        
-def test_multiple_quotes(db_session):
-    """Test adding multiple quotes to the database"""
+
+
+def test_multiple_quotes(db_session) -> None:
+
+    """
+    Test adding multiple quotes to the database
+
+    Returns: None
+
+    """
     multiple_quotes = [
-        Quote(quote="This is a test quote by Peter", author="Peter1", category="multiple_quote"),
-        Quote(quote="This is a test quote by Peter", author="Peter2", category="multiple_quote"),
-        Quote(quote="This is a test quote by Peter", author="Peter3", category="multiple_quote")
+        Quote(
+            quote="This is a test quote by Peter",
+            author="Peter1",
+            category="multiple_quote",
+        ),
+        Quote(
+            quote="This is a test quote by Peter",
+            author="Peter2",
+            category="multiple_quote",
+        ),
+        Quote(
+            quote="This is a test quote by Peter",
+            author="Peter3",
+            category="multiple_quote",
+        ),
     ]
     db_session.add_all(multiple_quotes)
     db_session.commit()
 
-    retrieved_quotes = db_session.query(Quote).filter(Quote.category == "multiple_quote").all()
+    retrieved_quotes = (
+        db_session.query(Quote).filter(Quote.category == "multiple_quote").all()
+    )
     assert len(retrieved_quotes) == 3
     retrieved_authors = set(quote.author for quote in retrieved_quotes)
     assert retrieved_authors == {"Peter1", "Peter2", "Peter3"}
 
-def test_empty_database(db_session):
-    """Test the database is empty"""
+
+def test_empty_database(db_session) -> None:
+
+    """
+    Test the database is empty
+
+    Returns: None
+
+    """
     db_session.query(Quote).delete()
     db_session.commit()
     retrieved_quotes = db_session.query(Quote).all()
     assert len(retrieved_quotes) == 0
-
-
-
-
-    
-
